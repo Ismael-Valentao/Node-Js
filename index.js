a9c7d83f26308dac5b83cd4ece81af20c11f27a3
Ismael-Valentao <chizianeismaelvalentao@gmail.com>
2024-02-12 09:08:38 +0200

    Getting Started

---
+++ index.js
@@ -0,0 +1,13 @@
+var express = require('express');
+
+var app = express();
+
+app.get('/', (req, res)=>{
+    res.status(200).write('<h1>Hello World!</h1>');
+})
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, ()=>{
+    console.log('Server Started...');
+});
+
